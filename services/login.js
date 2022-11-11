494901bb56c648ec3d0a2a4d43114de561c5308c
Prabin Basnet <prabinbasnet09@gmail.com>
2022-11-11 14:43:11 -0600

    Added login checks

--- services/login.js
+++ services/login.js
@@ -1,4 +1,6 @@
 const AWS = require('aws-sdk');
+const bcrypt = require('bcryptjs');
+
 AWS.config.update({
     region: 'us-east-1'
 })
@@ -7,7 +9,29 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 const tableName = 'chat-clubs'
 
 async function login(groupInfo){
+    const groupName = groupInfo.group_name;
+    const groupPassword = groupInfo.group_password;
 
+    group = {
+        TableName: tableName,
+        Key: {
+            group_name: groupName.trim()
+        }
+    }
+
+    const group = await dynamodb.get(group).promise().then(response => {
+        return response.Item
+    }, error => {
+        console.error('Server error',error)
+    })
+
+    if(!group || !group.groupName){
+        return util.buildResponse(403, 'Group does not exist')
+    }
+
+    if(!bcrypt.compareSync(groupPassword, group.group_password)){
+        return util.buildResponse(403, "Password is incorrect")
+    }
 } 
 
 module.exports = {login}
